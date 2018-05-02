9bf87604c3fa391701929ebfcfdfe025c9b84519
Siddiki <siddiki@cooper.edu>
2018-05-01 23:43:10 -0400

    receiver

--- 2018/receiver.py
+++ 2018/receiver.py
@@ -7,6 +7,7 @@ import utils
 
 
 class Receiver(object):
+    RE_DATA=bytearray()
 
     def __init__(self, inbound_port=50005, outbound_port=50006, timeout=10, debug_level=logging.INFO):
         self.logger = utils.Logger(self.__class__.__name__, debug_level)
@@ -19,8 +20,58 @@ class Receiver(object):
         self.simulator.sndr_setup(timeout)
 
     def receive(self):
-        raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
+        self.RE_DATA=self.simulator.u_receive()
+        self.send();
 
+
+    def send(self):
+        ackPackage=Segment()
+        ackPackage.ack(self.RE_DATA)
+        ackPackage.checksum=ackPackage.checkSum()
+        print ackPackage.checksum
+        print ackPackage.acknum
+        byteArray=bytearray([ackPackage.checksum,ackPackage.acknum])
+        self.simulator.u_send(byteArray)
+
+class Segment(object):
+    def __init__(self,checksum=0,seqnum=0,acknum=0,data=[]):
+        self.checksum = checksum
+        self.seqnum = seqnum
+        self.acknum = acknum
+        self.data = data
+
+    @staticmethod
+    def seqnum(self,lastseqnum,data):
+        return (lastseqnum + len(data))%255
+         
+    def checkSum(self):        #Since the segament of receiver only has ACK number in it, the checkSum value will just be the ACK number
+        return self.acknum
+        
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+    #@staticmethod
+    def checkCheckSum(self,data):        #this function calulates the checkSum of the RECEIVED data  
+        xorSum=~data[0]        #ReceivedCS should be an "one byte" object, the same type as xorSum in checkSum function
+        for i in xrange(len(data)):
+            xorSum^=data[i]
+            print data[i]
+            print xorSum
+        if xorSum==-127:         #if xorSum is 11111111, the data is not corrupted
+            return True
+        else:
+            return False
+    
+    #@staticmethod
+    def ack(self,data):
+        isGood = self.checkCheckSum(data)
+        if isGood:
+            self.acknum=(seqnum+len(data[3:]))%255
+        else:
+            pass
+    
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
 
 class BogoReceiver(Receiver):
     ACK_DATA = bytes(123)
@@ -39,5 +90,5 @@ class BogoReceiver(Receiver):
 
 if __name__ == "__main__":
     # test out BogoReceiver
-    rcvr = BogoReceiver()
+    rcvr = Receiver()
     rcvr.receive()
